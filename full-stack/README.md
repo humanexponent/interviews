735fdba6893d35fbcb3158f158754bdb7d107692
8adam95 <adam.szefer@gmail.com>
2020-08-07 00:18:23 +0200

    make backend work with frontend

--- full-stack/README.md
+++ full-stack/README.md
@@ -32,13 +32,16 @@ Test consists of multiple questions and each question has a set of possible answ
 Tests such as Big-5 (measuring personality scores, such as: Extraversion or Agreeableness) do not have a correct answer and each answer has some associated value e.g. answer representing option 'Strongly disagree' has value 1 and option 'Strongly Agree' has value 5.
 For simplicity let's assume that each score (extraversion, agreeableness, etc.) is calculated as the average of answers used for calculating this score. 
 Obviously, if we always display the same questions some users would try to game the test and get scores which would satisfy them. 
-To make their cheating more difficult we have a bigger set of question than it's required by test. For example for the example test, which requires only 10 questions, we have 24 possible questions. 
+To make their cheating more difficult we have a bigger set of questions than it's required by test. For example for the example test, which requires only 10 questions, we have 24 possible questions. 
 Additional feature would be to randomly shuffle quiz questions. 
 
-Your task is to generate a whole quiz with randomly picked questions in each index and then randomly shuffle quiz questions and pass them to front end.
-After user fills the test, calculate scores given information from quizTraits about which questions are needed to calculates scores and display results in a new page in a simple 'score': 'result' format.
+Your task is to generate a whole quiz with randomly picked questions in each index and then randomly shuffle quiz questions and pass them to front end. The way you structure quiz object (sent as a response) is up to you, but make it a single object that holds both questions and answers
 
-An example data is available in mockedData.js, please us it as a mocked database.
+After user completes the test, send it to backend. Again, structure of this post request is up to you. Then calculate scores in the backend given information from quizTraits about which questions are needed to calculate numerical scores. Then, on the frontend, display these scores on a new results page in a simple trait:numericalScore format.
+
+For example - if user answers 'Disagree' and 'Neither' to questions measuring Openness - he should see "Openness: 25%" on the result page. Why 25%? The average of his answers (0 and 1) is 0.5 which is 25% out of the highest possible score, which is 2.
+
+An example data is available in mockedData.js, please use it as a mocked database.
 
 
 ## Good to have
--- full-stack/mockedData.js
+++ full-stack/mockedData.js
@@ -1,60 +1,246 @@
-
 // Represents a quiz
-export const quizzes = [
-    { _id: 1, quizTitle: 'Test', public: true, visible: true, numberOfQuestions: 10}
-]
+const quizzes = [
+  {
+    _id: 1,
+    title: "Big-5 Personalities",
+    description: "Discover your personality based on the 10-item test",
+    numberOfQuestions: 10,
+  },
+];
 
 // Represents a quiz questions
-export const quizQuestions = [
-    {_id: 20, _quizId: 1, index: 1, quizQuestionTitle: 'Question1-1', quizQuestionAsnwers: [15, 16, 17]},
-    {_id: 22, _quizId: 1, index: 1, quizQuestionTitle: 'Question1-2', quizQuestionAsnwers: [15, 16, 17]},
-    {_id: 24, _quizId: 1, index: 1, quizQuestionTitle: 'Question1-3', quizQuestionAsnwers: [15, 16, 17]},
+const quizQuestions = [
+  {
+    _id: 20,
+    quizId: 1,
+    index: 1,
+    label: "I sympathize with others’ feelings.",
+    quizQuestionAnswers: [15, 16, 17],
+  },
+  {
+    _id: 22,
+    quizId: 1,
+    index: 1,
+    label: "Am the life of the party.",
+    quizQuestionAnswers: [15, 16, 17],
+  },
+  {
+    _id: 24,
+    quizId: 1,
+    index: 1,
+    label: "Get chores done right away.",
+    quizQuestionAnswers: [15, 16, 17],
+  },
+  {
+    _id: 26,
+    quizId: 1,
+    index: 2,
+    label: "I convince people to do things my way.",
+    quizQuestionAnswers: [10, 11, 12, 13, 14],
+  },
+  {
+    _id: 29,
+    quizId: 1,
+    index: 2,
+    label: "I believe in thinking things through.",
+    quizQuestionAnswers: [10, 11, 12, 13, 14],
+  },
 
-    {_id: 26, _quizId: 1, index: 2, quizQuestionTitle: 'Question2-1', quizQuestionAsnwers: [10, 11, 12, 13, 14]},
-    {_id: 29, _quizId: 1, index: 2, quizQuestionTitle: 'Question2-2', quizQuestionAsnwers: [10, 11, 12, 13, 14]},
+  {
+    _id: 33,
+    quizId: 1,
+    index: 3,
+    label: "I dislike changes.",
+    quizQuestionAnswers: [10, 11, 12, 13, 14],
+  },
+  {
+    _id: 35,
+    quizId: 1,
+    index: 3,
+    label: "I read all kinds of books.",
+    quizQuestionAnswers: [10, 11, 12, 13, 14],
+  },
+  {
+    _id: 37,
+    quizId: 1,
+    index: 3,
+    label: "I enjoy excitement.",
+    quizQuestionAnswers: [10, 11, 12, 13, 14],
+  },
 
-    {_id: 33, _quizId: 1, index: 3, quizQuestionTitle: 'Question3-1', quizQuestionAsnwers: [10, 11, 12, 13, 14]},
-    {_id: 35, _quizId: 1, index: 3, quizQuestionTitle: 'Question3-2', quizQuestionAsnwers: [10, 11, 12, 13, 14]},
-    {_id: 37, _quizId: 1, index: 3, quizQuestionTitle: 'Question3-3', quizQuestionAsnwers: [10, 11, 12, 13, 14]},
-    
-    {_id: 32, _quizId: 1, index: 4, quizQuestionTitle: 'Question4-1', quizQuestionAsnwers: [10, 11, 12, 13, 14]},
-    {_id: 34, _quizId: 1, index: 4, quizQuestionTitle: 'Question4-2', quizQuestionAsnwers: [10, 11, 12, 13, 14]},
-    {_id: 36, _quizId: 1, index: 4, quizQuestionTitle: 'Question4-3', quizQuestionAsnwers: [10, 11, 12, 13, 14]},
+  {
+    _id: 32,
+    quizId: 1,
+    index: 4,
+    label: "I prefer to save money rather than spend it.",
+    quizQuestionAnswers: [10, 11, 12, 13, 14],
+  },
+  {
+    _id: 34,
+    quizId: 1,
+    index: 4,
+    label: "I like to be reckless.",
+    quizQuestionAnswers: [10, 11, 12, 13, 14],
+  },
+  {
+    _id: 36,
+    quizId: 1,
+    index: 4,
+    label: "I care very little about what others say about me.",
+    quizQuestionAnswers: [10, 11, 12, 13, 14],
+  },
+  {
+    _id: 21,
+    quizId: 1,
+    index: 5,
+    label: "I get right to work when needed.",
+    quizQuestionAnswers: [15, 16, 17],
+  },
+  {
+    _id: 23,
+    quizId: 1,
+    index: 5,
+    label: "I often feel others are laughing at me.",
+    quizQuestionAnswers: [15, 16, 17],
+  },
+  {
+    _id: 25,
+    quizId: 1,
+    index: 5,
+    label: "I find it easy to forgive and forget.",
+    quizQuestionAnswers: [15, 16, 17],
+  },
 
-    {_id: 21, _quizId: 1, index: 5, quizQuestionTitle: 'Question5-1', quizQuestionAsnwers: [15, 16, 17]},
-    {_id: 23, _quizId: 1, index: 5, quizQuestionTitle: 'Question5-2', quizQuestionAsnwers: [15, 16, 17]},
-    {_id: 25, _quizId: 1, index: 5, quizQuestionTitle: 'Question5-3', quizQuestionAsnwers: [15, 16, 17]},
+  {
+    _id: 38,
+    quizId: 1,
+    index: 6,
+    label: "I have little concern for others.",
+    quizQuestionAnswers: [15, 16, 17],
+  },
+  {
+    _id: 39,
+    quizId: 1,
+    index: 6,
+    label: "It makes me feel good to do things out of revenge.",
+    quizQuestionAnswers: [15, 16, 17],
+  },
+  {
+    _id: 40,
+    quizId: 1,
+    index: 6,
+    label: "I can feel contact with a divine power.",
+    quizQuestionAnswers: [15, 16, 17],
+  },
 
-    {_id: 38, _quizId: 1, index: 6, quizQuestionTitle: 'Question6-1', quizQuestionAsnwers: [15, 16, 17]},
-    {_id: 39, _quizId: 1, index: 6, quizQuestionTitle: 'Question6-2', quizQuestionAsnwers: [15, 16, 17]},
-    {_id: 40, _quizId: 1, index: 6, quizQuestionTitle: 'Question6-3', quizQuestionAsnwers: [15, 16, 17]},
+  {
+    _id: 30,
+    quizId: 1,
+    index: 7,
+    label: "I was a good student back in school.",
+    quizQuestionAnswers: [10, 11, 12, 13, 14],
+  },
+  {
+    _id: 27,
+    quizId: 1,
+    index: 7,
+    label: "I was close to my parents in my childhood.",
+    quizQuestionAnswers: [10, 11, 12, 13, 14],
+  },
 
-    {_id: 30, _quizId: 1, index: 7, quizQuestionTitle: 'Question7-1', quizQuestionAsnwers: [10, 11, 12, 13, 14]},
-    {_id: 27, _quizId: 1, index: 7, quizQuestionTitle: 'Question7-2', quizQuestionAsnwers: [10, 11, 12, 13, 14]},
+  {
+    _id: 33,
+    quizId: 1,
+    index: 8,
+    label: "I feel worthless and hopeless.",
+    quizQuestionAnswers: [10, 11, 12, 13, 14],
+  },
+  {
+    _id: 35,
+    quizId: 1,
+    index: 8,
+    label: "I like science.",
+    quizQuestionAnswers: [10, 11, 12, 13, 14],
+  },
+  {
+    _id: 37,
+    quizId: 1,
+    index: 8,
+    label:
+      "I believe there's only one right answer to most questions.",
+    quizQuestionAnswers: [10, 11, 12, 13, 14],
+  },
 
-    {_id: 33, _quizId: 1, index: 8, quizQuestionTitle: 'Question8-1', quizQuestionAsnwers: [10, 11, 12, 13, 14]},
-    {_id: 35, _quizId: 1, index: 8, quizQuestionTitle: 'Question8-2', quizQuestionAsnwers: [10, 11, 12, 13, 14]},
-    {_id: 37, _quizId: 1, index: 8, quizQuestionTitle: 'Question8-3', quizQuestionAsnwers: [10, 11, 12, 13, 14]},
+  {
+    _id: 41,
+    quizId: 1,
+    index: 9,
+    label: "I can feel happy and sad at the same time.",
+    quizQuestionAnswers: [15, 16, 17],
+  },
+  {
+    _id: 42,
+    quizId: 1,
+    index: 9,
+    label: "I believe it is better to be safe than sorry.",
+    quizQuestionAnswers: [15, 16, 17],
+  },
+  {
+    _id: 43,
+    quizId: 1,
+    index: 9,
+    label: "It takes a lot to irritate me.",
+    quizQuestionAnswers: [15, 16, 17],
+  },
 
-    {_id: 41, _quizId: 1, index: 9, quizQuestionTitle: 'Question9-1', quizQuestionAsnwers: [15, 16, 17]},
-    {_id: 42, _quizId: 1, index: 9, quizQuestionTitle: 'Question9-2', quizQuestionAsnwers: [15, 16, 17]},
-    {_id: 43, _quizId: 1, index: 9, quizQuestionTitle: 'Question9-3', quizQuestionAsnwers: [15, 16, 17]},
-
-    {_id: 28, _quizId: 1, index: 10, quizQuestionTitle: 'Question10-1', quizQuestionAsnwers: [10, 11, 12, 13, 14]},
-    {_id: 31, _quizId: 1, index: 10, quizQuestionTitle: 'Question10-2', quizQuestionAsnwers: [10, 11, 12, 13, 14]},
+  {
+    _id: 28,
+    quizId: 1,
+    index: 10,
+    label: "I wanted to run away from school when I was a kid.",
+    quizQuestionAnswers: [10, 11, 12, 13, 14],
+  },
+  {
+    _id: 31,
+    quizId: 1,
+    index: 10,
+    label: "I like telling jokes.",
+    quizQuestionAnswers: [10, 11, 12, 13, 14],
+  },
 ];
 
 // Represents possible quiz question answers
-export const quizQuestionAsnwers = [
-    {_id: 10, quizQuestionAsnwerKey: '1', quizQuestionAsnwerValue: 'Strongly disagree'},
-    {_id: 11, quizQuestionAsnwerKey: '2', quizQuestionAsnwerValue: 'Disagree'},
-    {_id: 12, quizQuestionAsnwerKey: '3', quizQuestionAsnwerValue: 'Neither'}, 
-    {_id: 13, quizQuestionAsnwerKey: '4', quizQuestionAsnwerValue: 'Agree'},
-    {_id: 14, quizQuestionAsnwerKey: '5', quizQuestionAsnwerValue: 'Strongly Agree'},
-    {_id: 15, quizQuestionAsnwerKey: '1', quizQuestionAsnwerValue: 'Disagree'},
-    {_id: 16, quizQuestionAsnwerKey: '2', quizQuestionAsnwerValue: 'Neither'}, 
-    {_id: 17, quizQuestionAsnwerKey: '3', quizQuestionAsnwerValue: 'Agree'}
-]
+const quizQuestionAnswers = [
+  {
+    _id: 10,
+    value: "0",
+    label: "Strongly disagree",
+  },
+  { _id: 11, value: "1", label: "Disagree" },
+  { _id: 12, value: "2", label: "Neither" },
+  { _id: 13, value: "3", label: "Agree" },
+  {
+    _id: 14,
+    value: "4",
+    label: "Strongly Agree",
+  },
+  { _id: 15, value: "0", label: "Disagree" },
+  { _id: 16, value: "1", label: "Neither" },
+  { _id: 17, value: "2", label: "Agree" },
+];
 
 // Represents the relationship between score and related indexes of the quiz questions
-export const quizTrait = [{'openness': [1, 5]}, { 'extraversion': [2, 9]}, { 'impulsiveness': [3, 8]}, {'introversion': [6, 10]}, {'creativity': [4, 7]}]
+const quizTrait = [
+  { quizId: 1, trait: 'openness', answers: [1, 5] },
+  { quizId: 1, trait: 'extraversion', answers: [2, 9] },
+  { quizId: 1, trait: 'impulsiveness', answers: [3, 8] },
+  { quizId: 1, trait: 'introversion', answers: [6, 10] },
+  { quizId: 1, trait: 'creativity', answers: [4, 7] },
+];
+
+module.exports = {
+    quizzes,
+    quizQuestions,
+    quizQuestionAnswers,
+    quizTrait
+}
