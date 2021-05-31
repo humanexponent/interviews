ef06a9923aecccaa2fb183b6352960d1c229afb4
8adam95 <adam.szefer@gmail.com>
2021-05-31 18:27:27 +0200

    add mock data

---
+++ software-engineering-intern-python/mockData.py
@@ -0,0 +1,48 @@
+quizzes = [
+    {"_id": 1, "title": "Quiz #1"},
+    {"_id": 2, "title": "Quiz #2"},
+    {"_id": 3, "title": "Quiz #3"}
+    ]
+
+traits = [
+    {"_id": 10, "label": "Trait 1"}, 
+    {"_id": 11, "label": "Trait 2"}, 
+    {"_id": 12, "label": "Trait 3"}, 
+    {"_id": 13, "label": "Trait 4"}, 
+    {"_id": 14, "label": "Trait 5"}, 
+    {"_id": 15, "label": "Trait 6"},
+    {"_id": 16, "label": "Trait 7"},
+    {"_id": 17, "label": "Subtrait 8", "parentTraitId": 10},
+    {"_id": 18, "label": "Subtrait 9", "parentTraitId": 10},
+    {"_id": 19, "label": "Subtrait 10", "parentTraitId": 11},
+    {"_id": 20, "label": "Subtrait 11", "parentTraitId": 10}
+    {"_id": 21, "label": "Subtrait 12", "parentTraitId": 12},
+    {"_id": 22, "label": "Subtrait 13", "parentTraitId": 12},
+    {"_id": 23, "label": "Subtrait 14", "parentTraitId": 13},
+    {"_id": 24, "label": "Subtrait 15", "parentTraitId": 14}
+    {"_id": 25, "label": "Subtrait 16", "parentTraitId": 14},
+    {"_id": 26, "label": "Subtrait 17", "parentTraitId": 16},
+    {"_id": 27, "label": "Subtrait 18", "parentTraitId": 16},
+    {"_id": 28, "label": "Subtrait 19", "parentTraitId": 16}]
+
+questions = [
+    {"_id": 29, "quizId": 1, "traitId": 17, "label": "Question #1"}
+    {"_id": 30, "quizId": 1, "traitId": 18, "label": "Question #2"}
+    {"_id": 31, "quizId": 1, "traitId": 20, "label": "Question #3"}
+    {"_id": 32, "quizId": 1, "traitId": 20, "label": "Question #4"}
+    {"_id": 33, "quizId": 1, "traitId": 19, "label": "Question #5"}
+
+    {"_id": 34, "quizId": 2, "traitId": 21, "label": "Question #6"}
+    {"_id": 35, "quizId": 2, "traitId": 21, "label": "Question #7"}
+    {"_id": 36, "quizId": 2, "traitId": 21, "label": "Question #8"}
+
+    {"_id": 37, "quizId": 3, "traitId": 22, "label": "Question #9"}
+    {"_id": 38, "quizId": 3, "traitId": 23, "label": "Question #10"}
+    {"_id": 39, "quizId": 3, "traitId": 24, "label": "Question #11"}
+    {"_id": 40, "quizId": 3, "traitId": 25, "label": "Question #12"}
+    {"_id": 41, "quizId": 3, "traitId": 27, "label": "Question #13"}
+    {"_id": 42, "quizId": 3, "traitId": 22, "label": "Question #14"}
+    {"_id": 43, "quizId": 3, "traitId": 25, "label": "Question #15"}
+    {"_id": 44, "quizId": 3, "traitId": 22, "label": "Question #16"}
+]
+
